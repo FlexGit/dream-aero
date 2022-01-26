63ba343cb05e4f157126cf1c9976e875145d8136
Dmitry Sorochan <d1d1d1@inbox.ru>
2022-01-26 11:33:20 +0400

    api debug

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2027,7 +2027,8 @@ class ApiController extends Controller
 		if (!$contractor) {
 			return $this->responseError('Контрагент не найден', 400);
 		}
-		
+
+		\DB::connection()->enableQueryLog();
 		$events = Event::where('contractor_id', $contractorId)
 				->where('event_type', Event::EVENT_TYPE_DEAL)
 				->where('stop_at', '<', Carbon::now());
@@ -2037,6 +2038,8 @@ class ApiController extends Controller
 		$scores = Score::where('contractor_id', $contractorId)
 			->whereIn('event_id', $eventIds)
 			->get();
+		$queries = \DB::getQueryLog();
+		\Log::debug($queries);
 
 		$scoreData = [];
 		foreach ($scores ?? [] as $score) {
@@ -2049,8 +2052,9 @@ class ApiController extends Controller
 				'flight' => [
 					'date' => Carbon::parse($event->start_at)->format('Y-m-d'),
 					'time' => Carbon::parse($event->start_at)->format('H:i'),
-					'tariff' =>  ($event->position && $event->position->product) ? $event->position->product->format() : null,
+					'tariff' => ($event->position && $event->position->product) ? $event->position->product->format() : null,
 					'location' =>  $event->location ? $event->location->format() : null,
+					'flight_simulator' => $event->simulator ? $event->simulator->name : null,
 					'score' =>  $scoreData[$event->id] ?? 0,
 				],
 			];
