7e3ec0696726997cdfe48ffa2341241380cb31e1
Dmitry <d1d1d1@inbox.ru>
2022-02-09 00:32:14 +0300

    api fix

--- app/Http/Middleware/LogRequest.php
+++ app/Http/Middleware/LogRequest.php
@@ -3,10 +3,11 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class LogRequest
 {
-	public function handle($request, Closure $next)
+	public function handle(Request $request, Closure $next)
 	{
 		\Log::channel('api')->debug($request);
 		
