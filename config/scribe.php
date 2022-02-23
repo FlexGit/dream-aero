0da49eb9a52ca9aaf603806d51f399751e770433
Dmitry Sorochan <d1d1d1@inbox.ru>
2022-02-23 20:34:07 +0400

    api fix

--- config/scribe.php
+++ config/scribe.php
@@ -19,7 +19,7 @@ return [
     /*
      * The base URL displayed in the docs. If this is empty, Scribe will use the value of config('app.url').
      */
-    'base_url' => 'http://dev.dream-aero.ru', // http://dev.dream-aero.ru / http://admin.dream-aero.loc
+    'base_url' => 'https://dev.dream-aero.ru', // https://dev.dream-aero.ru / http://admin.dream-aero.loc
 
     /*
      * Tell Scribe what routes to generate documentation for.
@@ -183,7 +183,7 @@ return [
          * The base URL for the API tester to use (for example, you can set this to your staging URL).
          * Leave as null to use the current app URL (config(app.url)).
          */
-        'base_url' => 'http://dev.dream-aero.ru', // http://dev.dream-aero.ru / http://admin.dream-aero.loc
+        'base_url' => 'https://dev.dream-aero.ru', // https://dev.dream-aero.ru / http://admin.dream-aero.loc
 
         /**
          * Fetch a CSRF token before each request, and add it as an X-XSRF-TOKEN header. Needed if you're using Laravel Sanctum.
