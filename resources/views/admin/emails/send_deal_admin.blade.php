1dca603a8d29ede7e465613de52eafeb89273e2c
Dmitry Sorochan <d1d1d1@inbox.ru>
2022-03-07 13:10:27 +0400

    send deal admin letter template fix

--- resources/views/admin/emails/send_deal_admin.blade.php
+++ resources/views/admin/emails/send_deal_admin.blade.php
@@ -7,8 +7,8 @@
 <p>Тип сделки: {{ $isCertificatePurchase ? 'покупка сертификата' : 'бронирование' }}</p>
 <p>Статус сделки: {{ $statusName ?? '' }}</p>
 @if($isCertificatePurchase)
-	<p>Номер сертификата: {{ $certificateNumber ?? '' }}</p>
-	<p>Срок действия сертификата: @if($certificateExpireAt) {{ Carbon\Carbon::parse($certificateExpireAt)->format('d.m.Y') }} @else бессрочный @endif</p>
+	{{--<p>Номер сертификата: {{ $certificateNumber ?? '' }}</p>
+	<p>Срок действия сертификата: @if($certificateExpireAt) {{ Carbon\Carbon::parse($certificateExpireAt)->format('d.m.Y') }} @else бессрочный @endif</p>--}}
 	<p>Город действия сертификата: @if(!$cityName) все города России присутствия Dream Aero @else {{ $cityName }} @endif</p>
 @else
 	{{--@if($certificateNumber)
