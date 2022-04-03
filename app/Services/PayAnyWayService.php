ffe555c562338fcb13a3539c670dcce0dbbbe7aa
Dmitry <d1d1d1@inbox.ru>
2022-04-04 01:42:52 +0300

    payAnyWay debug

--- app/Services/PayAnyWayService.php
+++ app/Services/PayAnyWayService.php
@@ -44,20 +44,25 @@ class PayAnyWayService {
 					'handler' => $stack,
 				]
 			);
+			
+			$params = [
+				'MNT_ID' => $payAccountNumber,
+				'MNT_AMOUNT' => number_format($bill->amount, 2, '.', ''),
+				'MNT_TRANSACTION_ID' => $bill->number,
+				'MNT_CURRENCY_CODE' => self::CURRENCY_CODE,
+				'MNT_TEST_MODE' => self::TEST_MODE,
+				'MNT_DESCRIPTION' => 'Оплата по счету ' . $bill->number . ' на сумму ' . $bill->amount . ' ' . ($bill->currency ? $bill->currency->alias : 'RUB'),
+				'MNT_SUBSCRIBER_ID' => $bill->contractor->uuid,
+				'MNT_SIGNATURE' => md5($payAccountNumber . $bill->number . $bill->amount . self::CURRENCY_CODE . $bill->contractor->uuid . self::TEST_MODE . self::DATA_INTEGRITY_CHECK_CODE),
+				'MNT_SUCCESS_URL' => route('successPay'),
+				'MNT_FAIL_URL' => route('failPay'),
+				'MNT_RETURN_URL' => route('home'),
+			];
+			
+			\Log::debug($params);
+			
 			$result = $client->post(self::PAY_REQUEST_URL, [
-				'form_params' => [
-					'MNT_ID' => $payAccountNumber,
-					'MNT_AMOUNT' => number_format($bill->amount, 2, '.', ''),
-					'MNT_TRANSACTION_ID' => $bill->number,
-					'MNT_CURRENCY_CODE' => self::CURRENCY_CODE,
-					'MNT_TEST_MODE' => self::TEST_MODE,
-					'MNT_DESCRIPTION' => 'Оплата по счету ' . $bill->number . ' на сумму ' . $bill->amount . ' ' . ($bill->currency ? $bill->currency->alias : 'RUB'),
-					'MNT_SUBSCRIBER_ID' => $bill->contractor->uuid,
-					'MNT_SIGNATURE' => md5($payAccountNumber . $bill->number . $bill->amount . self::CURRENCY_CODE . $bill->contractor->uuid . self::TEST_MODE . self::DATA_INTEGRITY_CHECK_CODE),
-					'MNT_SUCCESS_URL' => route('successPay'),
-					'MNT_FAIL_URL' => route('failPay'),
-					'MNT_RETURN_URL' => route('home'),
-				],
+				'form_params' => $params,
 			]);
 			$httpCode = $result->getStatusCode();
 			$response = (string)$result->getBody()->getContents();
