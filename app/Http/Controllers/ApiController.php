10da63fbbaffc30f7181d87c79dbc2c22d81a094
Dmitry <d1d1d1@inbox.ru>
2022-05-09 12:27:04 +0300

    contractor notifications fix

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3224,11 +3224,12 @@ class ApiController extends Controller
 			}
 		}
 		
+		\DB::connection()->enableQueryLog();
 		$notifications = Notification::where('is_active', true)
 			->whereIn('contractor_id', [$contractor->id, 0])
 			->whereIn('city_id', [$city->id, 0])
 			->latest('created_at');
-		
+		\Log::debug(\DB::getQueryLog());
 		$data = [];
 		foreach ($notifications ?? [] as $notification) {
 			$data[] = [
