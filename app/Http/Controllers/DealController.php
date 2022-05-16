8995cb7c575fdcdeefbeac74ceb6ccd951ceddb9
Dmitry <d1d1d1@inbox.ru>
2022-05-17 02:53:25 +0300

    update

--- app/Http/Controllers/DealController.php
+++ app/Http/Controllers/DealController.php
@@ -229,7 +229,7 @@ class DealController extends Controller
 		$employees = User::where('enable', true)
 			->whereIn('city_id', [$user->city_id, 0])
 			->get();
-		dump($user);
+		\Log::debug($user);
 		$pilots = User::where('enable', true)
 			->whereIn('city_id', [$user->city_id, 0])
 			->where('role', User::ROLE_PILOT)
