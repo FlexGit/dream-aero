61bad46c4fa6979416effd25821c3b6e7c25ac7a
Dmitry <d1d1d1@inbox.ru>
2022-05-17 14:55:50 +0300

    update

--- app/Console/Commands/SendPromocodeAfterFlightEmail.php
+++ app/Console/Commands/SendPromocodeAfterFlightEmail.php
@@ -52,34 +52,44 @@ class SendPromocodeAfterFlightEmail extends Command
 			->whereBetween('stop_at', [Carbon::now()->startOfDay(), Carbon::now()->endOfDay()])
 			->get();
     	foreach ($events as $event) {
+    		\Log::debug('promocode - 1');
     		if (!$event->contractor_id) continue;
 		
+			\Log::debug('promocode - 2');
     		$location = $event->location;
     		if (!$location) continue;
 		
+			\Log::debug('promocode - 3');
 			$simulator = $event->simulator;
 			if (!$simulator) continue;
 		
+			\Log::debug('promocode - 4');
 			$deal = $event->deal;
 			if (!$deal) continue;
+			\Log::debug('promocode - 5');
 			if (!$deal->email) continue;
-
+		
+			\Log::debug('promocode - 6');
 			$position = $event->dealPosition;
 			if (!$position) continue;
-
+		
+			\Log::debug('promocode - 7');
 			$city = $event->city;
 			if (!$city) continue;
 		
+			\Log::debug('promocode - 8');
 			$contractor = Contractor::where('is_active', true)
 				->find($event->contractor_id);
 			if (!$contractor) continue;
-
+		
+			\Log::debug('promocode - 9');
 			// промокод данного типа контрагент может получить только единожды
 			$promocode = Promocode::where('is_active', true)
 				->where('contractor_id', $contractor->id)
 				->where('type', Promocode::SIMULATOR_TYPE)
 				->first();
 			if ($promocode) continue;
+			\Log::debug('promocode - 10');
 
 			// проверяем, есть ли в данном городе локация с другим типом авиатренажера
 			//\DB::connection()->enableQueryLog();
@@ -89,11 +99,13 @@ class SendPromocodeAfterFlightEmail extends Command
 				->first();
 			//\Log::debug(\DB::getQueryLog());
 			if (!$location) continue;
+			\Log::debug('promocode - 11');
 			
 			$anotherSimulator = FlightSimulator::where('is_active', true)
 				->where('id', '!=', $simulator->id)
 				->first();
 			if (!$anotherSimulator) continue;
+			\Log::debug('promocode - 12');
 
 			try {
 				$discount = HelpFunctions::getEntityByAlias(Discount::class, 'fixed_1000');
