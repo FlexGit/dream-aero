56c54b2c1a6986ac38a4fc065e365e09e11f169e
Dmitry <d1d1d1@inbox.ru>
2022-05-20 00:48:41 +0300

    update

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -208,7 +208,7 @@ class Event extends Model
 			$event->save();
 			
 			$eventComment = new EventComment();
-			$eventComment->name = 'Администратор ' . $event->user->fioFormatted();
+			$eventComment->name = 'Событие добавлено ' . $event->user->fioFormatted();
 			$eventComment->event_id = $event->id;
 			$eventComment->created_by = $event->user_id;
 			$eventComment->save();
