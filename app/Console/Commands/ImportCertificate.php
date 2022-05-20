790346f9350362e8063de363e8427bf334ae5348
Dmitry <d1d1d1@inbox.ru>
2022-05-20 18:32:39 +0300

    update

--- app/Console/Commands/ImportCertificate.php
+++ app/Console/Commands/ImportCertificate.php
@@ -39,7 +39,7 @@ class ImportCertificate extends Command
 	public function handle()
 	{
 		$this->output->title('Starting import');
-		(new CertificateImport)->withOutput($this->output)->import(storage_path('app/public/certificates4.xlsx'));
+		(new CertificateImport)->withOutput($this->output)->import(storage_path('app/public/certificates5.xlsx'));
 		$this->output->success('Import successful');
 	}
 }
