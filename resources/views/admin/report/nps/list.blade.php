cf3d9d70eae7f5e1ce69ec43aad0c302dbf6c8d5
Dmitry <d1d1d1@inbox.ru>
2022-05-24 00:34:39 +0300

    nps

--- resources/views/admin/report/nps/list.blade.php
+++ resources/views/admin/report/nps/list.blade.php
@@ -9,7 +9,7 @@
 					@if($user->city_id != $city->id || !isset($userNps[$user->id]))
 						@continue
 					@endif
-					<td class="align-top text-center" data-user-role="{{ $user->role }}">
+					<td class="align-top text-center" data-user-role="{{ $user->role }}" style="height: 100%;">
 						<table class="table table-hover table-sm">
 							<tr>
 								<td nowrap>{{ $user->fioFormatted() }}</td>
