71b579a9c48dbb982e00bad154e425a65ada8027
Dmitry <d1d1d1@inbox.ru>
2022-05-24 18:56:57 +0300

    master view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,9 +9,12 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<link rel="shortcut icon" type="image/png" href="{{ asset('img/favicon.png') }}" />
-
 	<meta name="google-site-verification" content="BHdHLHHg2mdgdi0sHcNT9Ng5yp2zThE-tl1tXxZZiGk" />
-	<meta name="yandex-verification" content="26119517b8383ec4" />
+	@if(app('\App')::isLocale('en'))
+		<meta name="yandex-verification" content="22062a6cc890fbc8" />
+	@else
+		<meta name="yandex-verification" content="26119517b8383ec4" />
+	@endif
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<script>
 		(function() {
