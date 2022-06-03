77d7a18682ec48d40dea82db027c0212c556681b
Dmitry <d1d1d1@inbox.ru>
2022-06-03 12:45:19 +0300

    certificate generate UNI fix

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -186,7 +186,7 @@ class Certificate extends Model
 			// vip
 			$certificateTemplateFilePath = 'certificate/template/VIP_' . mb_strtoupper($product->alias) . '.jpg';
 		} else {
-			$certificateTemplateFilePath = 'certificate/template/' . preg_replace("/[^A-Z]/", '', mb_strtoupper($product->alias)) . '_' . mb_strtoupper($city->alias) . '.jpg';
+			$certificateTemplateFilePath = 'certificate/template/' . preg_replace("/[^A-Z]/", '', mb_strtoupper($product->alias)) . '_' . $city ? mb_strtoupper($city->alias) : 'UNI' . '.jpg';
 		}
 		
 		if (!Storage::disk('private')->exists($certificateTemplateFilePath)) {
