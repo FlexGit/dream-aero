bd13d648599f5232ad4d914f9ba249edd01a506a
Dmitry <d1d1d1@inbox.ru>
2022-06-05 14:43:54 +0300

    aeroflot accrual export data type

--- app/Exports/AeroflotWriteOffReportExport.php
+++ app/Exports/AeroflotWriteOffReportExport.php
@@ -34,7 +34,7 @@ class AeroflotWriteOffReportExport implements FromView, WithColumnFormatting, Sh
 	public function columnFormats(): array
 	{
 		return [
-			'D' => \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING2,
+			'D' => \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING,
 			'G' => NumberFormat::FORMAT_NUMBER,
 			'H' => NumberFormat::FORMAT_NUMBER,
 			'I' => NumberFormat::FORMAT_NUMBER,
