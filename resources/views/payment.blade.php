18d1a44113190b8c275518ea73004152d246d667
Dmitry <d1d1d1@inbox.ru>
2022-06-05 23:42:57 +0300

    aeroflot payment form

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -14,78 +14,81 @@
 			<div class="article-content">
 				<div class="row">
 					<div class="item">
-						@if($error)
-							<p>{{ $error }}</p>
-						@else
-							<div class="popup popup-newreservation" id="popup">
-								{{--<span style="color: green;">@lang('main.pay.redirect')</span>--}}
-								{!! $html !!}
-								<label for="name">Имя</label>
-								<input type="text" id="name" value="{{ $deal->name }}" class="popup-input" readonly style="font-size: 14px;">
-								<label for="amount">Сумма к оплате</label>
-								<input type="text" id="amount" value="{{ $bill->amount }}" class="popup-input" readonly style="font-size: 14px;">
-								<input type="hidden" id="product" value="{{ ($bill->position && $bill->position->product) ? $bill->position->product->id : 0 }}">
-								@if($bill)
-									@if($bill->aeroflot_transaction_type == app('\App\Services\AeroflotBonusService')::TRANSACTION_TYPE_REGISTER_ORDER)
-										@if($bill->aeroflot_state == app('\App\Services\AeroflotBonusService')::PAYED_STATE)
-											<div style="color: #56BA76;font-weight: 600;margin: 10px;">Ваша скидка подтверждена и после использования миль составила {{ $bill->aeroflot_bonus_amount }} рублей</div>
-										@elseif($bill->aeroflot_state == app('\App\Services\AeroflotBonusService')::REGISTERED_STATE)
-											<div style="color:#ff8200;font-weight:600;margin:10px">Ваша скидка в размере {{ $bill->aeroflot_bonus_amount }} рублей пока не подтверждена. Пожалуйста, подождите и обновите статус позже!</div>
-											<ul class="aerbonus_btns" style="padding-inline-start: 10px;">
-												<li class="js-use-retry" data-uuid="{{ $bill->uuid }}">Повторить попытку</li>
-												<li class="js-status-refresh" data-uuid="{{ $bill->uuid }}">Обновить статус</li>
-											</ul>
-										@elseif($bill->aeroflot_state == app('\App\Services\AeroflotBonusService')::CANCEL_STATE)
-											<div style="color: red;font-weight: 600;margin: 10px">Извините, Ваша скидка в размере {{ $bill->aeroflot_bonus_amount }} рублей была отклонена!</div>
-											<ul class="aerbonus_btns" style="padding-inline-start: 10px;">
-												<li class="js-use-retry" data-uuid="{{ $bill->uuid }}">Повторить попытку</li>
-											</ul>
-										@endif
-									@else
-										@if($_SERVER['REMOTE_ADDR'] == '79.165.99.239')
-										@if(($bill->position->product && $bill->position->product->productType && in_array($bill->position->product->productType->alias, [app('\App\Models\ProductType')::REGULAR_ALIAS, app('\App\Models\ProductType')::ULTIMATE_ALIAS, app('\App\Models\ProductType')::COURSES_ALIAS]) && ($bill->position->product->alias != 'fly_no_fear')) || !$bill->position->product)
-											<div class="aeroflot_container" style="margin-left: 0;margin-right: 0;">
-												<div style="display: flex;">
-													<div class="switch_box" style="align-items: normal;margin-bottom: 0;">
-														<label class="switch" style="margin-top: 3px;margin-right: 10px;">
-															<input type="checkbox" name="has_aeroflot_card" class="edit_field" value="1">
-															<span class="slider round"></span>
-														</label><span style="font-size: 14px;">@lang('main.modal-certificate.есть-карта-аэрофлот')</span>
-													</div>
-													<div style="display: flex;width: 100%;">
-														<div style="width: 100%;">
-															<input type="text" id="aeroflot_card" name="aeroflot_card" class="popup-input" placeholder="@lang('main.modal-certificate.введите-номер-карты-аэрофлот')" style="display: none;margin-bottom: 0;padding-top: 0;padding-bottom: 0;font-size: 13px;">
+						<fieldset>
+							@if($error)
+								<p>{{ $error }}</p>
+							@else
+								<div class="popup popup-newreservation" id="popup">
+									{{--<span style="color: green;">@lang('main.pay.redirect')</span>--}}
+									{!! $html !!}
+									<label for="name">Имя</label>
+									<input type="text" id="name" value="{{ $deal->name }}" class="popup-input" readonly style="font-size: 14px;">
+									<label for="amount">Сумма к оплате</label>
+									<input type="text" id="amount" value="{{ $bill->amount }}" class="popup-input" readonly style="font-size: 14px;">
+									@if($bill)
+										@if($bill->aeroflot_transaction_type == app('\App\Services\AeroflotBonusService')::TRANSACTION_TYPE_REGISTER_ORDER)
+											@if($bill->aeroflot_state == app('\App\Services\AeroflotBonusService')::PAYED_STATE)
+												<div style="color: #56BA76;font-weight: 600;margin: 10px;">Ваша скидка подтверждена и после использования миль составила {{ $bill->aeroflot_bonus_amount }} рублей</div>
+											@elseif($bill->aeroflot_state == app('\App\Services\AeroflotBonusService')::REGISTERED_STATE)
+												<div style="color:#ff8200;font-weight:600;margin:10px">Ваша скидка в размере {{ $bill->aeroflot_bonus_amount }} рублей пока не подтверждена. Пожалуйста, подождите и обновите статус позже!</div>
+												<ul class="aerbonus_btns" style="padding-inline-start: 10px;">
+													<li class="js-use-retry" data-uuid="{{ $bill->uuid }}">Повторить попытку</li>
+													<li class="js-status-refresh" data-uuid="{{ $bill->uuid }}">Обновить статус</li>
+												</ul>
+											@elseif($bill->aeroflot_state == app('\App\Services\AeroflotBonusService')::CANCEL_STATE)
+												<div style="color: red;font-weight: 600;margin: 10px">Извините, Ваша скидка в размере {{ $bill->aeroflot_bonus_amount }} рублей была отклонена!</div>
+												<ul class="aerbonus_btns" style="padding-inline-start: 10px;">
+													<li class="js-use-retry" data-uuid="{{ $bill->uuid }}">Повторить попытку</li>
+												</ul>
+											@endif
+										@else
+											@if($_SERVER['REMOTE_ADDR'] == '79.165.99.239')
+											@if(($bill->position->product && $bill->position->product->productType && in_array($bill->position->product->productType->alias, [app('\App\Models\ProductType')::REGULAR_ALIAS, app('\App\Models\ProductType')::ULTIMATE_ALIAS, app('\App\Models\ProductType')::COURSES_ALIAS]) && ($bill->position->product->alias != 'fly_no_fear')) || !$bill->position->product)
+													<input type="hidden" id="product" value="{{ ($bill->position && $bill->position->product) ? $bill->position->product->id : 0 }}">
+
+													<div class="aeroflot_container" style="margin-left: 0;margin-right: 0;">
+													<div style="display: flex;">
+														<div class="switch_box" style="align-items: normal;margin-bottom: 0;">
+															<label class="switch" style="margin-top: 3px;margin-right: 10px;">
+																<input type="checkbox" name="has_aeroflot_card" class="edit_field" value="1">
+																<span class="slider round"></span>
+															</label><span style="font-size: 14px;">@lang('main.modal-certificate.есть-карта-аэрофлот')</span>
 														</div>
-														<button type="button" class="popup-submit popup-small-button button-pipaluk button-pipaluk-orange js-aeroflot-card-btn" style="display: none;width: 45px;height: 33px;"><i>Ok</i></button>
-														<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" class="close-btn js-aeroflot-card-remove" style="display: none;margin: 6px 0 0 2px;"><path d="M12 10.587l6.293-6.294a1 1 0 111.414 1.414l-6.293 6.295 6.293 6.294a1 1 0 11-1.414 1.414L12 13.416 5.707 19.71a1 1 0 01-1.414-1.414l6.293-6.294-6.293-6.295a1 1 0 111.414-1.414L12 10.587z" fill="currentColor"></path></svg>
+														<div style="display: flex;width: 100%;">
+															<div style="width: 100%;">
+																<input type="text" id="aeroflot_card" name="aeroflot_card" class="popup-input" placeholder="@lang('main.modal-certificate.введите-номер-карты-аэрофлот')" style="display: none;margin-bottom: 0;padding-top: 0;padding-bottom: 0;font-size: 13px;">
+															</div>
+															<button type="button" class="popup-submit popup-small-button button-pipaluk button-pipaluk-orange js-aeroflot-card-btn" style="display: none;width: 45px;height: 33px;"><i>Ok</i></button>
+															<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" class="close-btn js-aeroflot-card-remove" style="display: none;margin: 6px 0 0 2px;"><path d="M12 10.587l6.293-6.294a1 1 0 111.414 1.414l-6.293 6.295 6.293 6.294a1 1 0 11-1.414 1.414L12 13.416 5.707 19.71a1 1 0 01-1.414-1.414l6.293-6.294-6.293-6.295a1 1 0 111.414-1.414L12 10.587z" fill="currentColor"></path></svg>
+														</div>
 													</div>
+													<small class="aeroflot_note" style="display: none;font-size: 80%;">* @lang('main.modal-certificate.введите-номер-карты-аэрофлот-описание')</small>
+													<div class="aeroflot-buttons-container"></div>
 												</div>
-												<small class="aeroflot_note" style="display: none;font-size: 80%;">* @lang('main.modal-certificate.введите-номер-карты-аэрофлот-описание')</small>
-												<div class="aeroflot-buttons-container"></div>
-											</div>
-										@endif
+											@endif
+											@endif
 										@endif
 									@endif
-								@endif
 
-								<div class="aeroflot-loader"></div>
+									<div class="aeroflot-loader"></div>
 
-								<div class="consent-container">
-									<label class="cont">
-										@lang('main.modal-certificate.с-правилами-и-условиями-оферты-ознакомлен', ['link_rules' => url('rules-dreamaero'), 'link_offer' => url('oferta-dreamaero')])
-										<input type="checkbox" name="consent" value="1">
-										<span class="checkmark" style="margin-top: 5px;"></span>
-									</label>
+									<div class="consent-container">
+										<label class="cont">
+											@lang('main.modal-certificate.с-правилами-и-условиями-оферты-ознакомлен', ['link_rules' => url('rules-dreamaero'), 'link_offer' => url('oferta-dreamaero')])
+											<input type="checkbox" name="consent" value="1">
+											<span class="checkmark" style="margin-top: 5px;"></span>
+										</label>
+									</div>
+
+									<div style="margin-top: 10px;margin-left: 18px;margin-right: 18px;">
+										<div class="alert alert-success hidden" role="alert"></div>
+										<div class="alert alert-danger hidden" role="alert"></div>
+									</div>
+
+									<button type="button" class="popup-submit button-pipaluk button-pipaluk-grey js-pay-btn" style="margin-top: 20px;" disabled><i>@lang('main.common.оплатить')</i></button>
 								</div>
-
-								<div style="margin-top: 10px;margin-left: 18px;margin-right: 18px;">
-									<div class="alert alert-success hidden" role="alert"></div>
-									<div class="alert alert-danger hidden" role="alert"></div>
-								</div>
-
-								<button type="button" class="popup-submit button-pipaluk button-pipaluk-grey js-pay-btn" style="margin-top: 20px;" disabled><i>@lang('main.common.оплатить')</i></button>
-							</div>
-						@endif
+							@endif
+						</fieldset>
 					</div>
 				</div>
 			</div>
