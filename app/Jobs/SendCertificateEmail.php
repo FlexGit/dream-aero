51669e333bf6a8e02f1948fb659a3436596f981f
Dmitry <d1d1d1@inbox.ru>
2022-06-21 23:21:59 +0300

    send sertificate email

--- app/Jobs/SendCertificateEmail.php
+++ app/Jobs/SendCertificateEmail.php
@@ -82,19 +82,19 @@ class SendCertificateEmail extends Job implements ShouldQueue {
 		$certificateRulesFile = Image::make($certificateRulesTemplateFilePath)->encode('jpg');
 
 		$fontPath = public_path('assets/fonts/Montserrat/Montserrat-Medium.ttf');
-		$certificateRulesFile->text($period, 341, 250, function ($font) use ($fontPath) {
+		$certificateRulesFile->text($period, 340, 250, function ($font) use ($fontPath) {
 			$font->file($fontPath);
 			$font->size(20);
 			$font->color('#000000');
 		});
-		$certificateRulesFile->text($peopleCount, 782, 312, function ($font) use ($fontPath) {
+		$certificateRulesFile->text($peopleCount, 783, 312, function ($font) use ($fontPath) {
 			$font->file($fontPath);
 			$font->size(20);
 			$font->color('#000000');
 		});
 		
 		$fontPath = public_path('assets/fonts/Montserrat/Montserrat-ExtraBold.ttf');
-		$certificateRulesFile->text($cityPhone ?? '', 635, 408, function ($font) use ($fontPath) {
+		$certificateRulesFile->text($cityPhone ?? '', 636, 408, function ($font) use ($fontPath) {
 			$font->file($fontPath);
 			$font->size(20);
 			$font->color('#000000');
