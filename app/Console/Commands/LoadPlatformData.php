af13620ff8184d142872a204d2002e924c5ec704
Dmitry <d1d1d1@inbox.ru>
2022-06-30 10:46:32 +0300

    platform load data

--- app/Console/Commands/LoadPlatformData.php
+++ app/Console/Commands/LoadPlatformData.php
@@ -62,7 +62,7 @@ class LoadPlatformData extends Command
 		
 		/** @var \Webklex\PHPIMAP\Query\WhereQuery $query */
 		/** @var \Webklex\PHPIMAP\Support\MessageCollection $messages */
-		$messages = $query->/*unseen()->*/since(Carbon::now()->subDay())->get();
+		$messages = $query->/*unseen()->*/since(Carbon::now()->subWeek())->get();
 	
 		/** @var \Webklex\PHPIMAP\Message $message */
 		foreach ($messages as $message) {
