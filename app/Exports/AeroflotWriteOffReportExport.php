b3172abbadc0a9bc8b0128ee9c36dc34d541e73d
Dmitry <d1d1d1@inbox.ru>
2022-08-31 11:12:10 +0300

    aeroflot reports export columns format

--- app/Exports/AeroflotWriteOffReportExport.php
+++ app/Exports/AeroflotWriteOffReportExport.php
@@ -31,19 +31,7 @@ class AeroflotWriteOffReportExport extends DefaultValueBinder  implements FromVi
 	
 	public function bindValue(Cell $cell, $value)
 	{
-		if ($cell->getColumn() == 'G' && is_int($value)) {
-			\Log::debug($value);
-			$cell->setValueExplicit($value, DataType::TYPE_NUMERIC);
-			
-			return true;
-		}
-		if ($cell->getColumn() == 'H' && is_int($value)) {
-			$cell->setValueExplicit($value, DataType::TYPE_NUMERIC);
-			
-			return true;
-		}
-		
-		if ($cell->getColumn() == 'I' && is_int($value)) {
+		if (in_array($cell->getColumn(), ['D','G','H','I']) && is_int($value)) {
 			$cell->setValueExplicit($value, DataType::TYPE_NUMERIC);
 			
 			return true;
