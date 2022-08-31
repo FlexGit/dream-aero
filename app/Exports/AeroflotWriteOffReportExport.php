7cc4f3a341a659766428660d8d4bdd0f427a2933
Dmitry <d1d1d1@inbox.ru>
2022-08-31 11:32:40 +0300

    aeroflot reports export columns format

--- app/Exports/AeroflotWriteOffReportExport.php
+++ app/Exports/AeroflotWriteOffReportExport.php
@@ -3,9 +3,12 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
-use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder;
 
 class AeroflotWriteOffReportExport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder implements FromView, ShouldAutoSize, WithCustomValueBinder
 {
@@ -24,5 +27,17 @@ class AeroflotWriteOffReportExport extends \PhpOffice\PhpSpreadsheet\Cell\String
 	public function array(): array
 	{
 		return $this->data;
+	}
+	
+	public function bindValue(Cell $cell, $value)
+	{
+		/*if (in_array($cell->getColumn(), ['G','H','I']) && is_int($value)) {
+			$cell->setValueExplicit($value, DataType::TYPE_NUMERIC);
+			
+			return true;
+		}*/
+		
+		// else return default behavior
+		return parent::bindValue($cell, $value);
 	}
 }
