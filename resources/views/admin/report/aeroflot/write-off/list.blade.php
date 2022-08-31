09a06cf5ef3ef2ba9cc6d338004c9fa7cb066649
Dmitry <d1d1d1@inbox.ru>
2022-08-31 11:14:58 +0300

    aeroflot reports export columns format

--- resources/views/admin/report/aeroflot/write-off/list.blade.php
+++ resources/views/admin/report/aeroflot/write-off/list.blade.php
@@ -27,7 +27,7 @@
 					{{ $item['location_name'] }}
 				</td>
 				<td class="align-middle text-center" style="text-align: center;vertical-align: middle;">
-					{{ (int) $item['transaction_order_id'] }}
+					{{ $item['transaction_order_id'] }}
 				</td>
 				<td class="align-middle text-center" style="text-align: center;vertical-align: middle;">
 					{{ $item['transaction_created_at'] }}
@@ -36,13 +36,13 @@
 					{{ $item['card_number'] }}
 				</td>
 				<td class="align-middle text-center" style="text-align: center;vertical-align: middle;">
-					{{ (int) $item['bill_amount'] }}
+					{{ $item['bill_amount'] }}
 				</td>
 				<td class="align-middle text-center" style="text-align: center;vertical-align: middle;">
-					{{ (int) $item['bonus_amount'] }}
+					{{ $item['bonus_amount'] }}
 				</td>
 				<td class="align-middle text-center" style="text-align: center;vertical-align: middle;">
-					{{ (int) $item['bonus_miles'] }}
+					{{ $item['bonus_miles'] }}
 				</td>
 				<td class="align-middle text-center" style="text-align: center;vertical-align: middle;">
 					{{ $item['product_type_name'] }}
