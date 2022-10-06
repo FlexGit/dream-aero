b547ed72c2e746ff8a22e92e6bcc0f33f0959cf2
Dmitry <d1d1d1@inbox.ru>
2022-10-06 13:27:16 +0300

    birthday promo

--- resources/views/admin/emails/birthday_promo.blade.php
+++ resources/views/admin/emails/birthday_promo.blade.php
@@ -18,9 +18,54 @@
 										<b>Здравствуйте{{ $contractor->name ? ', ' . $contractor->name : '' }}!</b>
 									</p>
 									<p style="margin:15px 30px 33px;text-align:left;font-size:14px;line-height:30px;color:#484a42;">
-										В преддверии Вашего Дня Рождения мы рады напомнить, что у Вас есть возможность воспользоваться скидкой {{ ($promo->discount->valueFormatted() ?? '') }} на полет в Авиатренажере. <a href="https://dream-aero.ru/{{ $city ? $city->alias : 'msk' }}/price?utm_source=dreamaero&utm_medium=email&utm_campaign=birthday_promo" style="color:#ffffff;font:bold 16px 'arial' , 'helvetica neue' , 'helvetica' , sans-serif;text-decoration:none" target="_blank" rel="noopener noreferrer">Подробнее</a>.
+										В преддверии Вашего Дня Рождения мы рады напомнить, что у Вас есть возможность воспользоваться скидкой {{ ($promo->discount->valueFormatted() ?? '') }} на полет в Авиатренажере.
 										<br>
 									</p>
+									<table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="100%" style="background:#fff;border-collapse:collapse;color:#333;font-size:16px;line-height:1.5">
+										<tbody>
+										<tr style="border-color:transparent">
+											<td style="border-collapse:collapse;border-color:transparent">
+												<table cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse;color:#333;font-size:16px;line-height:1.5">
+													<tbody>
+													<tr style="border-color:transparent">
+														<td colspan="3" height="15" width="100%" style="border-collapse:collapse;border-color:transparent">&nbsp;</td>
+													</tr>
+													<tr style="border-color:transparent">
+														<td height="100%" width="15" style="border-collapse:collapse;border-color:transparent;width:15px !important">&nbsp;</td>
+														<td valign="top" width="370" style="border-collapse:collapse;border-color:transparent;vertical-align:top">
+															<div>
+																<table border="0" cellpadding="0" cellspacing="0" height="28" style="background:#888;border:0;border-collapse:collapse;border-radius:5px;color:#333;font-size:16px;line-height:1.5;margin-left:auto;margin-right:auto">
+																	<tbody>
+																	<tr style="border-color:transparent">
+																		<td width="21" style="border:0 transparent;border-collapse:collapse;padding:0;width:21px">&nbsp;&nbsp;</td>
+																		<td align="center" height="28" valign="middle" style="border:0 transparent;border-collapse:collapse;border-radius:5px;height:28px;padding:0;text-align:center;vertical-align:middle;width:auto">
+																			<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border:0;border-collapse:collapse;color:#333;font-size:16px;line-height:1.5">
+																				<tbody>
+																				<tr style="border-color:transparent">
+																					<td align="center" style="border:0 transparent;border-collapse:collapse;line-height:1;padding:0">
+																						<a href="https://dream-aero.ru/{{ $city ? $city->alias : 'msk' }}/price?utm_source=dreamaero&utm_medium=email&utm_campaign=birthday_promo" style="color:#ffffff;font:bold 16px 'arial' , 'helvetica neue' , 'helvetica' , sans-serif;text-decoration:none" target="_blank" rel="noopener noreferrer">Подробнее</a>
+																					</td>
+																				</tr>
+																				</tbody>
+																			</table>
+																		</td>
+																		<td width="21" style="border:0 transparent;border-collapse:collapse;padding:0;width:21px">&nbsp;&nbsp;</td>
+																	</tr>
+																	</tbody>
+																</table>
+															</div>
+														</td>
+														<td height="100%" width="15" style="border-collapse:collapse;border-color:transparent;width:15px !important">&nbsp;</td>
+													</tr>
+													<tr style="border-color:transparent">
+														<td colspan="3" height="15" width="100%" style="border-collapse:collapse;border-color:transparent">&nbsp;</td>
+													</tr>
+													</tbody>
+												</table>
+											</td>
+										</tr>
+										</tbody>
+									</table>
 									<p style="border-top:2px solid #e5e5e5;font-size:5px;line-height:5px;margin:0 30px 29px;">&nbsp;</p>
 									<table cellpadding="0" cellspacing="0" border="0" width="100%">
 										<tbody>
