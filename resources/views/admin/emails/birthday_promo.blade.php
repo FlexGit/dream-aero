a22a794d7fe2fb642d4b1a9bcf6b6461d3748820
Dmitry <d1d1d1@inbox.ru>
2022-10-06 16:45:59 +0300

    birthday promo

--- resources/views/admin/emails/birthday_promo.blade.php
+++ resources/views/admin/emails/birthday_promo.blade.php
@@ -10,7 +10,7 @@
 							<tbody>
 							<tr>
 								<td width="4" height="4"><p style="margin:0;font-size:1px;line-height:1px;">&nbsp;</p></td>
-								<td colspan="3" rowspan="3" bgcolor="#FFFFFF" style="padding:10px 0 30px;text-align:center">
+								<th colspan="3" rowspan="3" bgcolor="#FFFFFF" style="padding:10px 0 30px;text-align:center">
 									<a href="https://dream-aero.ru" style="display:flex;width:167px;height:auto;margin:0 auto;" target="_blank" rel=" noopener noreferrer">
 										<img src="https://dream-aero.ru/assets/img/logo-new-mail.png" width="172px" alt="logo" style="display:flex;border:0;margin:0;">
 									</a>
@@ -18,54 +18,13 @@
 										<b>Здравствуйте{{ $contractor->name ? ', ' . $contractor->name : '' }}!</b>
 									</p>
 									<p style="margin:15px 30px 33px;text-align:left;font-size:14px;line-height:30px;color:#484a42;">
-										В преддверии Вашего Дня Рождения мы рады напомнить, что у Вас есть возможность воспользоваться скидкой {{ ($promo->discount->valueFormatted() ?? '') }} на полет в Авиатренажере.
-										<br>
+										Поздравляем с наступающим Днем Рождения! Спешим напомнить, что с сегодняшнего дня и до {{ \Carbon\Carbon::parse($contractor->birthdate)->addDays(3)->format('d.m.Y') }} у вас есть возможность забронировать полет со скидкой {{ ($promo->discount->valueFormatted() ?? '') }} в честь праздника.
+										<br><br>
+										На подарочные сертификаты с открытой датой акция не распространяется.
 									</p>
-									<table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="100%" style="background:#fff;border-collapse:collapse;color:#333;font-size:16px;line-height:1.5">
-										<tbody>
-										<tr style="border-color:transparent">
-											<td style="border-collapse:collapse;border-color:transparent">
-												<table cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse;color:#333;font-size:16px;line-height:1.5">
-													<tbody>
-													<tr style="border-color:transparent">
-														<td colspan="3" height="15" width="100%" style="border-collapse:collapse;border-color:transparent">&nbsp;</td>
-													</tr>
-													<tr style="border-color:transparent">
-														<td height="100%" width="15" style="border-collapse:collapse;border-color:transparent;width:15px !important">&nbsp;</td>
-														<td valign="top" width="370" style="border-collapse:collapse;border-color:transparent;vertical-align:top">
-															<div>
-																<table border="0" cellpadding="0" cellspacing="0" height="28" style="background:#888;border:0;border-collapse:collapse;border-radius:5px;color:#333;font-size:16px;line-height:1.5;margin-left:auto;margin-right:auto">
-																	<tbody>
-																	<tr style="border-color:transparent">
-																		<td width="21" style="border:0 transparent;border-collapse:collapse;padding:0;width:21px">&nbsp;&nbsp;</td>
-																		<td align="center" height="28" valign="middle" style="border:0 transparent;border-collapse:collapse;border-radius:5px;height:28px;padding:0;text-align:center;vertical-align:middle;width:auto">
-																			<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border:0;border-collapse:collapse;color:#333;font-size:16px;line-height:1.5">
-																				<tbody>
-																				<tr style="border-color:transparent">
-																					<td align="center" style="border:0 transparent;border-collapse:collapse;line-height:1;padding:0">
-																						<a href="https://dream-aero.ru/{{ $city ? $city->alias : 'msk' }}/price?utm_source=dreamaero&utm_medium=email&utm_campaign=birthday_promo" style="color:#ffffff;font:bold 16px 'arial' , 'helvetica neue' , 'helvetica' , sans-serif;text-decoration:none" target="_blank" rel="noopener noreferrer">Подробнее</a>
-																					</td>
-																				</tr>
-																				</tbody>
-																			</table>
-																		</td>
-																		<td width="21" style="border:0 transparent;border-collapse:collapse;padding:0;width:21px">&nbsp;&nbsp;</td>
-																	</tr>
-																	</tbody>
-																</table>
-															</div>
-														</td>
-														<td height="100%" width="15" style="border-collapse:collapse;border-color:transparent;width:15px !important">&nbsp;</td>
-													</tr>
-													<tr style="border-color:transparent">
-														<td colspan="3" height="15" width="100%" style="border-collapse:collapse;border-color:transparent">&nbsp;</td>
-													</tr>
-													</tbody>
-												</table>
-											</td>
-										</tr>
-										</tbody>
-									</table>
+									<table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color:#fff;border-bottom-left-radius:0;border-bottom-right-radius:0;border-collapse:collapse;border-color:transparent;border-top-left-radius:0;border-top-right-radius:0;font-size:16px;line-height:1.5;margin-bottom:0 !important"><tbody><tr style="border-color:transparent"><td style="border-collapse:collapse;border-color:transparent"><table cellpadding="0" cellspacing="0" width="100%" style="border-collapse:separate;border-color:transparent;font-size:16px;line-height:1.5;margin-bottom:0 !important;overflow:hidden"><tbody><tr style="border-color:transparent;color:#363333;font-family:'tahoma' , 'geneva' , sans-serif"><td valign="top" width="570" style="border-collapse:collapse;border-color:transparent;padding:15px;vertical-align:top;word-break:break-word"><div><table border="0" cellpadding="0" cellspacing="0" style="background:#0b2387;border:0 transparent;border-collapse:collapse;border-radius:5px;box-sizing:border-box;font-size:16px;line-height:1.5;margin-bottom:0 !important;margin-left:auto;margin-right:auto;width:auto !important"><tbody style="box-sizing:border-box"><tr style="border-color:transparent;box-sizing:border-box"><td align="center" height="40" valign="middle" style="border:0 transparent;border-collapse:collapse;border-radius:5px;box-sizing:border-box;height:40px;padding:0;text-align:center;vertical-align:middle;width:auto"><table border="0" cellpadding="0" cellspacing="0" width="100%" style="border:0 transparent;border-collapse:collapse;box-sizing:border-box;font-size:16px;line-height:1.5;margin-bottom:0 !important"><tbody><tr style="border-color:transparent;box-sizing:border-box"><td align="center" style="border:0 transparent;border-collapse:collapse;box-sizing:border-box;line-height:1;padding:0"><a href="https://dream-aero.ru/{{ $city ? $city->alias : 'msk' }}/price?utm_source=dreamaero&utm_medium=email&utm_campaign=birthday_promo" target="_blank" style="box-sizing:border-box;color:#ffffff;font-family:'arial' , 'helvetica neue' , 'helvetica' , sans-serif;font-size:16px;font-weight:bold;padding:12px 18px 12px 18px;text-decoration:none" rel=" noopener noreferrer">Забронировать со скидкой</a></td></tr></tbody></table></td></tr></tbody></table></div><div style="box-sizing:border-box;clear:both;font-size:16px;line-height:1.5">&nbsp;</div></td></tr></tbody></table></td></tr></tbody></table>
+									<table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color:#fff;border-bottom-left-radius:0;border-bottom-right-radius:0;border-collapse:collapse;border-color:transparent;border-top-left-radius:0;border-top-right-radius:0;font-size:16px;line-height:1.5;margin-bottom:0 !important"><tbody><tr style="border-color:transparent"><td style="border-collapse:collapse;border-color:transparent"><div style="font-size:16px;line-height:1.5;padding:15px"><hr style="border-style:solid none none none;border-top-color:#b2b2b2;border-width:1px 0 0 0;margin:0"></div></td></tr></tbody></table>
+									<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse;border-color:transparent;font-size:16px;line-height:1.5;margin-bottom:0 !important;width:100%"><tbody><tr style="border-color:transparent"><th align="left" valign="top" width="600" style="border-color:transparent;font-weight:400;text-align:left;vertical-align:top"><table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="100%" style="background-color:#fff;border-bottom-left-radius:0;border-bottom-right-radius:0;border-collapse:collapse;border-color:transparent;border-top-left-radius:0;border-top-right-radius:0;font-size:16px;line-height:1.5;margin-bottom:0 !important"><tbody><tr style="border-color:transparent"><td style="border-collapse:collapse;border-color:transparent"><table cellpadding="0" cellspacing="0" width="100%" style="border-collapse:separate;border-color:transparent;font-size:16px;line-height:1.5;margin-bottom:0 !important;overflow:hidden"><tbody><tr style="border-color:transparent;color:#363333;font-family:'tahoma' , 'geneva' , sans-serif"><td valign="top" width="570" style="border-collapse:collapse;border-color:transparent;padding:15px;vertical-align:top;word-break:break-word"><div align="center" style="box-sizing:border-box;font-size:16px;height:365;line-height:1.5;text-align:center;width:100%"><img src="https://dream-aero.ru/assets/img/email/birthday.jpg" alt="" style="max-width:100%"></div></td></tr></tbody></table></td></tr></tbody></table></th></tr></tbody></table>
 									<p style="border-top:2px solid #e5e5e5;font-size:5px;line-height:5px;margin:0 30px 29px;">&nbsp;</p>
 									<table cellpadding="0" cellspacing="0" border="0" width="100%">
 										<tbody>
