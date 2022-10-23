5c3fdd1dad8fc80cf1716f7fde0c2810194cf925
Dmitry <d1d1d1@inbox.ru>
2022-10-23 17:03:57 +0300

    contractor search discount

--- app/Http/Controllers/ContractorController.php
+++ app/Http/Controllers/ContractorController.php
@@ -421,7 +421,7 @@ class ContractorController extends Controller
 			$status = $contractor->getStatus($statuses, $flightTime);
 			
 			$suggestions[] = [
-				'value' => $contractor->name . ($contractor->lastname ? ' ' . $contractor->lastname : '') . ' [' . $contractor->email . ($contractor->phone ? ', ' . $contractor->phone : '') . ($contractor->city ? ', ' . $contractor->city->name : '') . (($status && $status->discount) ? ', скидка ПГ ' . $status->discount->valueFormatted() : '') . ']',
+				'value' => $contractor->name . ($contractor->lastname ? ' ' . $contractor->lastname : '') . ' [' . $contractor->email . ($contractor->phone ? ', ' . $contractor->phone : '') . ($contractor->city ? ', ' . $contractor->city->name : '') . (($status && $status->discount) ? ', скидка ПГ: ' . $status->discount->valueFormatted() : 'нет') . ']',
 				'id' => $contractor->id,
 				'data' => [
 					'name' => $contractor->name,
