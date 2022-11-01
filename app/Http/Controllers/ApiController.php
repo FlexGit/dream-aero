439e85bcc7584990dbe4ac6a9af3d53a76d2747d
Dmitry <d1d1d1@inbox.ru>
2022-11-01 14:30:33 +0300

    debug

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -236,12 +236,11 @@ class ApiController extends Controller
 			
 			$subject = env('APP_NAME') . ': код подтверждения';
 			
-			Mail::send('admin.emails.code', $messageData, function ($message) use ($subject, $recipients) {
+			/*Mail::send('admin.emails.code', $messageData, function ($message) use ($subject, $recipients) {*/
 				/** @var \Illuminate\Mail\Message $message */
-				$message->from('pr@dream-aero.ru');
-				$message->subject($subject);
+				/*$message->subject($subject);
 				$message->to($recipients);
-			});
+			});*/
 			
 			$failures = Mail::failures();
 			//Log::debug($_SERVER['REMOTE_ADDR'] . ': ' . implode(' ', $failures));
