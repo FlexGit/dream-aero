47b7daa0228c39d994b587501e2aa85919a17243
Dmitry <d1d1d1@inbox.ru>
2022-11-01 18:12:22 +0300

    mail send failures log

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => 'pr@dream-aero.ru',
+        'name' => '"Dream Aero"',
     ],
 
     /*
