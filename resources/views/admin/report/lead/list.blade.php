c56f656ab258bb4090e9fe4a555f161c5cf3e597
Dmitry <d1d1d1@inbox.ru>
2022-11-22 15:33:49 +0300

    lead report

--- resources/views/admin/report/lead/list.blade.php
+++ resources/views/admin/report/lead/list.blade.php
@@ -27,25 +27,25 @@
 	<tbody>
 	@foreach($leads as $lead)
 		<tr>
-			<td class="align-middle text-center">
+			<td class="align-middle text-center text-nowrap">
 				{{ isset($types[$lead->type]) ? $types[$lead->type] : '' }}
 			</td>
-			<td class="align-middle text-center">
+			<td class="align-middle text-center text-nowrap">
 				{{ $lead->city ? $lead->city->name : '' }}
 			</td>
-			<td class="align-middle text-center">
+			<td class="align-middle text-center text-nowrap">
 				{{ $lead->name }}
 			</td>
-			<td class="align-middle text-center">
+			<td class="align-middle text-center text-nowrap">
 				{{ $lead->email }}
 			</td>
-			<td class="align-middle text-center">
+			<td class="align-middle text-center text-nowrap">
 				{{ $lead->phone }}
 			</td>
-			<td class="align-middle text-center">
+			<td class="align-middle text-center text-nowrap">
 				{{ $lead->product ? $lead->product->name : '' }}
 			</td>
-			<td class="align-middle text-center">
+			<td class="align-middle text-center text-nowrap">
 				{{ \Carbon\Carbon::parse($lead->created_at)->format('Y-m-d H:i') }}
 			</td>
 		</tr>
